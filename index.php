f6aed0d7c2fb8f70ad604e8ba0e4190ad69c53e2
Ai Sugimoto <i.sugimoto1112@gmail.com>
2015-11-10 22:37:52 +0900

    メソッドの宣言、->の宣言

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <?php
 
 //クラスを宣言→インスタンス化
-
+//メソッド = クラス内で宣言された関数
 
 class Car
 {
 	public $gasoline = 30;//プロパティ
+
+	public function go()//メソッドの宣言
+	{
+		echo '車が走りました!';
+		$this->gasoline--;//そのクラス内での関数にアクセス出来る。
+		echo '残りのガソリンは'. $this->gasoline . 'Lです<br>';
+	}
 }
 
 $myCar = new Car;//Carというクラス(設計図)を元にしてインスタンスを作る。
 
-echo $myCar->gasoline;
+$myCar->go();
+
+for ($i = 0; $i < 50; $i++)
+{
+    $myCar->go();
+}
